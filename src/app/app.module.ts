91d367b57fdb67956125922ffc270785c469159f
RosaMedina28 <rosamedina1008@gmail.com>
2023-06-12 20:12:09 -0600

    [ADD] components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CommunityComponent } from './components/community/community.component';
+import { ActivitiesComponent } from './components/activities/activities.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    CommunityComponent,
+    ActivitiesComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
