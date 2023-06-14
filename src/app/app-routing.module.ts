166c456425bc00d21771b01d69b847a0736a4115
RosaMedina28 <rosamedina1008@gmail.com>
2023-06-14 12:46:34 -0600

    [ADD] components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SplashComponent } from './components/splash/splash.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ActivitiesComponent } from './components/activities/activities.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'splash', pathMatch: 'full' },
   { path: 'home', component: NavbarComponent },
   { path: 'splash', component: SplashComponent },
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'activities', component: ActivitiesComponent}
+
 ];
 
 @NgModule({
